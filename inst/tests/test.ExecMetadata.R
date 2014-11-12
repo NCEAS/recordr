dc134311bcf3be209de41f3084abc387a6b07c91
Peter Slaughter <slaughter@nceas.ucsb.edu>
2014-11-12 09:14:39 -0800

    Added tests for ExecMetadata methods

---
+++ inst/tests/test.ExecMetadata.R
@@ -0,0 +1,32 @@
+testthat::context("ExecMetadata tests")
+
+library(recordr)
+
+scriptPath <- tempfile(pattern = "file", tmpdir = tempdir(), fileext = ".R")
+
+test_that("Can create an ExecMetadaa object", {
+  # Check that tests have been setup
+  execMeta <- ExecMetadata(scriptPath)
+  expect_that(execMeta, is_a("ExecMetadata"))
+  expect_that(execMeta@softwareApplication, equals(scriptPath))
+})
+
+test_that("Can persist an ExecMetadaa object", {
+  rc <- Recordr()
+  execMeta <- ExecMetadata(scriptPath)
+  filePath <- writeExecMeta(rc, execMeta)
+  expect_that(file.exists(filePath), is_true())
+  expect_that(file.info(filePath)["size"], is_more_than(0))
+})
+
+test_that("Can retrieve an ExecMetadata object", {
+  rc <- Recordr()
+  mdf <- listRuns(rc, quiet = TRUE)
+  if (nrow(mdf) == 0) {
+    warning("Unable to test ExecMetadata retrieval because no runs have been recorded")
+  } else {
+    execId <- mdf[1, "executionId"]
+    execMeta <- readExecMeta(rc, execId)
+    expect_equal(execMeta@executionId, execId)
+  }
+})
