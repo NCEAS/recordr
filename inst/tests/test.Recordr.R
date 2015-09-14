c299e5207cd85234038be380d097120520e0cfcf
Peter Slaughter <slaughter@nceas.ucsb.edu>
2015-09-14 16:25:36 -0700

    Remove session config tests

--- inst/tests/test.Recordr.R
+++ inst/tests/test.Recordr.R
@@ -88,47 +88,4 @@ test_that("Can delete a script execution", {
   expect_that(oneRow, is_true())
 })
 
-test_that("Can read and write configuration parameters", {
-  library(uuid)
-  uuidTag <- UUIDgenerate()
-  # Check that tests have been setup
-  expect_that(class(uuidTag), equals("character"))
-  recordr <- new("Recordr")  
-  
-  # Shouldn't be able to read config params unless we are in an active recording session
-  err <- try(readConfig(recordr), silent=TRUE)
-  expect_that(class(err), (matches("try-error")))
-  # Test startRecord() / endRecord()
-  executionId <- startRecord(recordr, tag=uuidTag)
-  
-  # Read the sample configuration that is stored with the installed package. Parameters 
-  # will be placed in the ".recordrConfig" environment
-  loadConfig(recordr)
-  expect_that(length(base::ls(".recordrConfig")), is_more_than(0))
-  # Check that we can change a configuration value
-  val1 <- getConfig(recordr, name="public_read_allowed")
-  setConfig(recordr, "public_read_allowed", !val1)
-  val2 <- getConfig(recordr, name="public_read_allowed")
-  expect_that(val1 != val2, is_true())
-  
-  err <- try(setConfig(recordr, "public_read_allowed", 1), silent=TRUE)
-  expect_that(class(err), (matches("try-error")))
-  
-  # Check that we can write out a configuration
-  configFile <- tempfile(pattern = "file", tmpdir = tempdir(), fileext = ".R")
-  saveConfig(recordr, file=configFile)
-  expect_that(file.exists(configFile), is_true())
-  found <- grep("public_read_allowed", readLines(configFile))
-  expect_that(found, is_more_than(0))
-  endRecord(recordr)
-  
-  # Check that we can read in a stored configuration
-  uuidTag <- UUIDgenerate()
-  executionId <- startRecord(recordr, tag=uuidTag, config=configFile)
-  val3 <- getConfig(recordr, name="public_read_allowed")
-  endRecord(recordr)
-  expect_that(val2 == val3, is_true())
-  unlink(configFile)
-
-})
 
