e8ce5dbc5860ba87bbef4ad03077955030cb36ff
Peter Slaughter <slaughter@nceas.ucsb.edu>
2015-10-02 11:36:47 -0700

    Add example script and data

---
+++ inst/extdata/EmCoverage.R
@@ -0,0 +1,19 @@
+library(dataone)
+library(ggplot2)
+# This test script can be used to test the 'record' method of the recordr package.
+# This script downloads a dataset from DataONE and creates a local CSV file from
+# this dataset.
+# The provenance relationships that should be recorded are:
+#     Endocladia_muricata.csv < wasGeneratedBy <- plotCoverages.R
+#     plotCoverages.R <- used <- coverages_2001-2010.csv
+#
+# Here is a new comment
+df <- read.csv(file = system.file("./extdata/coverages_2001-2010.csv", package="recordr"))
+endocladia_coverage <- df[df$final_classification=="endocladia muricata",]
+myDir <- tempdir()
+csvOutFile <- sprintf("%s/Endocladia_muricata.csv", myDir)
+write.csv(endocladia_coverage, file = csvOutFile)
+
+plotFile <- sprintf("%s/emCoverage.png", myDir)
+qplot(em$latitude.dd., em$longitude.dd.)
+ggsave(plotFile, plot=last_plot())
