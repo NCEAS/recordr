a7051b7a55e1f45787da3729420c91677f64f2b7
Peter Slaughter <slaughter@nceas.ucsb.edu>
2015-10-03 09:52:45 -0700

    Fix axes of plot generated by sample script

--- inst/extdata/EmCoverage.R
+++ inst/extdata/EmCoverage.R
@@ -15,5 +15,6 @@ csvOutFile <- sprintf("%s/Endocladia_muricata.csv", myDir)
 write.csv(endocladia_coverage, file = csvOutFile)
 
 plotFile <- sprintf("%s/emCoverage.png", myDir)
-qplot(em$latitude.dd., em$longitude.dd.)
+qplot(em$longitude.dd., em$latitude.dd., xlab="longitude", ylab="latitude",
+       main="Endocladia muricata distribution"))
 ggsave(plotFile, plot=last_plot())
